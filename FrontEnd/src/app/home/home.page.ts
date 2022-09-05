e4759aa02c8ea0e9e1103103da56e62c83ca6577
Rafael-Carriel <rafaredesociais@gmail.com>
2022-09-04 23:10:37 -0300

    olha a merda acontecendo

--- FrontEnd/src/app/home/home.page.ts
+++ FrontEnd/src/app/home/home.page.ts
@@ -1,12 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  nickname : string;
+  email : string;
 
-  constructor() {}
+  constructor(private alertController : AlertController) {}
 
+  ngOnInit(){
+      this.nickname;
+      this.email;
+  }
+  AlertInput(){
+
+  }
+  async presentAlertRegister() {
+    const alert = await this.alertController.create({
+      header: 'Cadastrar',
+      buttons: [
+        {
+          text: 'Cancelar',
+          
+        },
+        {
+          text: 'Cadastrar',
+        },
+      ],
+      inputs: [     
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          name : "nickname",
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: "email",
+          placeholder: 'Email',
+          name: "email",
+        },
+        {
+          type: "password",
+          placeholder: "Senha",
+          name: "senha",
+          attributes: {
+            minlenght: 6,
+            maxlenght: 9,
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+  async presentAlertLogin() {
+    const alert = await this.alertController.create({
+      header: 'Cadastrar',
+      buttons: [
+        {
+          text: 'Cancelar',
+          
+        },
+        {
+          text: 'Cadastrar',
+        },
+      ],
+      inputs: [     
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          name : "nickname",
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: "email",
+          placeholder: 'Email',
+          name: "email",
+        },
+        {
+          type: "password",
+          placeholder: "Senha",
+          name: "senha",
+          attributes: {
+            minlenght: 6,
+            maxlenght: 9,
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
